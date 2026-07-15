d78130008fb8908cbee9c64ca86484b1f5683113
Nabil K Sabu <nabilksabu55@gmail.com>
2026-07-15 20:44:43 +0530

    added some files

--- flappy.py
+++ flappy.py
@@ -7,12 +7,16 @@ screen = pygame.display.set_mode((288, 512))
 clock = pygame.time.Clock()
 running = True
 dt = 2
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pygame.Vector2((screen.get_width() / 2) - 25, screen.get_height() / 2)
 
 bg_img = pygame.image.load(Path("assets") / "Game Objects" / "background-day.png")
+
 flappy_bird = pygame.image.load("assets/Game Objects/yellowbird-downflap.png").convert()
+
 bird_width = flappy_bird.get_width()
 bird_height = flappy_bird.get_height()
+gravity = 0.5
+ground = 512
 
 pygame.display.set_caption("Flappy Bird")
 
@@ -27,17 +31,11 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w]:
+    if keys[pygame.K_SPACE]:
         player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    player_pos.x = max(0, min(player_pos.x, screen.get_width() - bird_width))
-    player_pos.y = max(0, min(player_pos.y, screen.get_height() - bird_height))
+    if player_pos.x:
+        player_pos.x = max(0, min(player_pos.x, screen.get_width() - bird_width))
+        player_pos.y = max(0, min(player_pos.y, screen.get_height() - bird_height))
 
     dt = clock.tick(60) / 1000
     pygame.display.flip()
